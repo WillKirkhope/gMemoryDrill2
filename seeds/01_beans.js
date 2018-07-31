8958d4bcf192aa533ab1b6336df4f9dd590e6567
WillKirkhope <kirkhopetraining@yahoo.com>
2018-07-31 14:49:24 -0600

    Added Migrations and seeds

---
+++ seeds/01_beans.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex, Promise) {
+  return knex('coffee').del()
+    .then(() => {
+      return knex('coffee').insert([
+        {name: 'Black and Tan', roaster: 'Ink', aroma: 3},
+        {name: 'Holiday Roast', roaster: 'Starbucks', aroma: 9},
+        {name: 'House Quake', roaster: 'Denver Coffee', aroma: 6}
+      ]);
+    }).then(() => {
+      return knex.raw("ALTER SEQUENCE coffee_id_seq RESTART WITH 4;");
+  });
+};
